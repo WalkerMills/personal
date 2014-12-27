2753ba4a6b9aa3e13b44d5c8f38c0ffcdcbc79ba
Walker Mills <walker.mills7@gmail.com>
2014-12-27 02:58:32 -0800

    Adds connect & disconnect methods to link
    Links can now be connected & disconnected properly.  Also defined
    equality by value & a skeleton send method on the *Transport type.

--- go/src/github.com/WalkerMills/simulator/link/link.go
+++ go/src/github.com/WalkerMills/simulator/link/link.go
@@ -5,23 +5,54 @@ import (
     "simulator/packet"
 )
 
-const (
-    UP int = 0
-    DOWN int = 1
-)
 
 type Link struct {
     id int
     capacity int
     size int
     delay int
-    endpoints [2]*node.Receiver
+    connected [2]*node.Receiver
     buffers [2][]*packet.Routable
 }
 
-type Transport struct {
-    direction int
-    handler *Link
+// TODO: Link constructor
+
+func (l *Link) handler(direction int) *Transport {
+    return &Transport{target: 1 - direction, link: l}
 }
 
-// TODO: add link buffers flushing at rate Link.capacity, handling methods
+func (l *Link) connect(A *node.Receiver, B *node.Receiver) {
+    // Connect the endpoints
+    l.connected[direction] := [2]*node.Receiver{A, B}
+    // Give the endpoints transport handlers
+    for direction, endpoint := range l.connected {
+        go l.connected[direction].connect(l.handler(direction))
+    }
+}
+
+func (l *Link) disconnect() {
+    // Tell the receivers to disconnect
+    for i, endpoint := range l.connected {
+        go endpoint.disconnect(l.handler(i))
+    }
+    // Disconnect the endpoints
+    l.connected := [2]*node.Receiver{nil, nil}
+}
+
+func (l *Link) enqueue(dest int, p *packet.Routable) {
+    // TODO: rate limit packets (CoDel)
+}
+
+
+type Transport {
+    target int
+    link *Link
+}
+
+func (t *Transport) eq(other *Transport) bool {
+    return t.link == other.link && t.target == other.target
+}
+
+func (t *Transport) send(p *packet.Routable) {
+    t.link.enqueue(t.target, p)
+}
